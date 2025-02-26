c5b342fc9d7e5088b82811eaf7282dd87bfa5389
Deryck Ting <deryckgod36@gmail.com>
2025-02-26 17:52:54 +0800

    修復Bug成功連線資料庫

--- web/src/main/java/com/divination/web/controllers/HomeController.java
+++ web/src/main/java/com/divination/web/controllers/HomeController.java
@@ -1,6 +1,7 @@
 package com.divination.web.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class HomeController {
